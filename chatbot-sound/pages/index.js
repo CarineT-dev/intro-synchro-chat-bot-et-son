bd40f04adb5d25049e6157e9f39048034ee6db66
CarineT-dev <74300213+CarineT-dev@users.noreply.github.com>
2021-02-15 15:42:29 +0100

    chat and song

---
+++ chatbot-sound/pages/index.js
@@ -0,0 +1,47 @@
+
+import Head from 'next/head'
+import styles from '../styles/Home.module.scss'
+import { Howl } from 'howler';
+
+const monSonAJouer = new Howl({
+  src: 'click.mp3',
+  autoplay: false
+
+})
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <section className="nes-container">
+        <section className='message-list'>
+          <section className="message -left">
+            <i className="nes-bcrikko" ></i>
+            <div className="nes-balloon from-left">
+              <p>Hello ness.css</p>
+
+            </div>
+
+          </section>
+
+          <section className="message -right">
+
+            <div className="nes-balloon from-right">
+              <p>Hello ness.css Hello ness.css Hello ness.css</p>
+
+            </div>
+            <i className="nes-bcrikko" ></i>
+          </section>
+
+        </section>
+
+
+      </section>
+      <button onClick={() => monSonAJouer.play()} className='nes-btn is-primary'>Click Me</button>
+
+
+    </div>
+  )
+}
